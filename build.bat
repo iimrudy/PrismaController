9ffb923b5adca100f478dbae5e1971c603286520
Luca <54636488+iimrudy@users.noreply.github.com>
2022-05-03 18:43:31 +0200

    Removed packr/v2

--- build.bat
+++ build.bat
@@ -1,8 +1,5 @@
 @echo off
 echo Building PrismaController.
-packr2 clean
-packr2
 go build -ldflags -H=windowsgui .
-packr2 clean
 cls
-echo Prisma Controller Builded.
+echo Prisma Controller Compiled.
